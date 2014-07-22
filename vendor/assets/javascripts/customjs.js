3d984920874ab40c100b5e9864713f9d2319b70d
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-07-22 15:58:15 -0400

    js and about page

---
+++ vendor/assets/javascripts/customjs.js
@@ -0,0 +1,20 @@
+<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+
+//    <script>
+// jQuery(document).ready(function() {
+//       // script to type text in header
+//       var text = 'This is test content';
+
+//       // text is split up to letters
+//       $.each(text.split(''), function(i, letter){
+
+//           // add 100*i ms delay to each letter 
+//           setTimeout(function(){
+
+//               // add the letter to the container
+//               $('.about h4').html($('.about h4').html() + letter);
+
+//           }, 100*i);
+//       });
+// });
+//   </script>
