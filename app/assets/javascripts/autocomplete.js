f96fab66d4b448ca8c08532d64f8103fade2361d
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-03 18:35:04 -0400

    commented out js autocomplete for work later

--- app/assets/javascripts/autocomplete.js
+++ app/assets/javascripts/autocomplete.js
@@ -1,7 +1,7 @@
-$(document).ready(function (){
-  var availableTags_questions = <%= (Question.all.collect { |question| "#{question.title}" }) %>;
+// $(document).ready(function (){
+//   var availableTags_questions = <%= (Question.all.collect { |question| "#{question.title}" }) %>;
 
-  $('#autocomplete').autocomplete({source: availableTags_questions });
-// {source: availableTags_questions }
-  // alert("jquery working ");
-});
+//   $('#autocomplete').autocomplete({source: availableTags_questions });
+// // {source: availableTags_questions }
+//   // alert("jquery working ");
+// });
