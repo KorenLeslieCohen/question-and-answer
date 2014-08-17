06c64a60f78c0170f83eb3883085f460f3cfb241
Koren Leslie Cohen <koren.cohen@gmail.com>
2014-08-17 19:10:54 -0400

    css tweaks

--- app/assets/javascripts/autocomplete.js
+++
@@ -1,7 +0,0 @@
-// $(document).ready(function (){
-//   var availableTags_questions = <%= (Question.all.collect { |question| "#{question.title}" }) %>;
-
-//   $('#autocomplete').autocomplete({source: availableTags_questions });
-// // {source: availableTags_questions }
-//   // alert("jquery working ");
-// });
